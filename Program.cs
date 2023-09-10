39d0f942abff8cdfb7fb88b99066ffc425a2bdd8
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-10 15:41:00 +0800

    20. Postman

--- Program.cs
+++ Program.cs
@@ -25,18 +25,24 @@ internal class Program
         {
             case "GET":
             {
-                var containsKeys = context.Request.Query.ContainsKey("id");
                 var containsAgent = context.Request.Headers.ContainsKey("User-Agent");
+                
                 if (containsAgent)
                 {
                     var userAgent = context.Request.Headers["User-Agent"];
                     await context.Response.WriteAsync($"<p>Hello World UserAgent! {userAgent}</p>");
                 }
-
+                var containsKeys = context.Request.Query.ContainsKey("id");
                 if (containsKeys)
                 {
                     var id = context.Request.Query["id"].ToString();
                     await context.Response.WriteAsync($"<p>Hello World ID! {id}</p>");
+                }
+                var containsAuthorizationKey = context.Request.Headers.ContainsKey("AuthorizationKey");
+                if (containsAuthorizationKey)
+                {
+                    var auth = context.Request.Headers["AuthorizationKey"].ToString();
+                    await context.Response.WriteAsync($"<p>Hello World containsAuthorizationKey! {auth}</p>");
                 }
 
                 break;
