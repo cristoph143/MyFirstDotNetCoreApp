bec1c1b7c27d85f822c85b843c4aa4c38c74a2d2
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-10 15:18:35 +0800

    18. Query Strings

--- Program.cs
+++ Program.cs
@@ -10,16 +10,34 @@ internal class Program
         {
             string path = context.Request.Path;
             string method = context.Request.Method;
-            context.Response.Headers["MyKey"] = "myValue";
-            context.Response.Headers.Add("MyKey2", "myValue2");
-            context.Response.Headers["Content-Type"] = "text/html";
-            context.Response.StatusCode = 400;
-            context.Response.StatusCode = 1 == 1 ? 200 : 400;
+            await queryString(method, context);
             await context.Response.WriteAsync("Hello World1!");
             await context.Response.WriteAsync("<h1>Hello World1!</h1>");
             await context.Response.WriteAsync($"<p>Hello World1! {path} ? {method}</p>");
             await context.Response.WriteAsync("Hello World2!");
         });
         app.Run();
+    }
+
+    private static async Task queryString(string method, HttpContext context)
+    {
+        switch (method)
+        {
+            case "GET":
+            {
+                var containsKeys = context.Request.Query.ContainsKey("id");
+                switch (containsKeys)
+                {
+                    case true:
+                    {
+                        var id = context.Request.Query["id"].ToString();
+                        await context.Response.WriteAsync($"<p>Hello World ID! {id}</p>");
+                        break;
+                    }
+                }
+
+                break;
+            }
+        }
     }
 }
