221b0292068fe82586fd976fab82d808eea80f49
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-10 15:00:14 +0800

    16. HTTP Response Headers

--- Program.cs
+++ Program.cs
@@ -8,9 +8,13 @@ internal class Program
         app.MapGet("/", () => "Hello World!");
         app.Run(async context =>
         {
+            context.Response.Headers["MyKey"] = "myValue";
+            context.Response.Headers.Add("MyKey2", "myValue2");
+            context.Response.Headers["Content-Type"] = "text/html";
             context.Response.StatusCode = 400;
             context.Response.StatusCode = 1 == 1 ? 200 : 400;
             await context.Response.WriteAsync("Hello World1!");
+            await context.Response.WriteAsync("<h1>Hello World1!</h1>");
             await context.Response.WriteAsync("Hello World2!");
         });
         app.Run();
