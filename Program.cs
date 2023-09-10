1dcdc18093502763d0d6a5136fe13b170db1c973
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-10 14:02:02 +0800

    15. HTTP Status Code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+        app.MapGet("/", () => "Hello World!");
+        app.Run(async context =>
+        {
+            context.Response.StatusCode = 400;
+            context.Response.StatusCode = 1 == 1 ? 200 : 400;
+            await context.Response.WriteAsync("Hello World1!");
+            await context.Response.WriteAsync("Hello World2!");
+        });
+        app.Run();
+    }
+}
