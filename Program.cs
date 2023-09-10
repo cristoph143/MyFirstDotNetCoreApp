a85da9fad0a62783c8656f2a2256bc1dc762c703
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-10 22:43:17 +0800

    middleware baseline

--- Program.cs
+++ Program.cs
@@ -10,14 +10,20 @@ internal abstract class Program
         app.MapGet("/hello", () => "Hello World!");
         app.Run(async context =>
         {
-            // await queryString(context);
-            var reader = new StreamReader(context.Request.Body);
-            var body = await reader.ReadToEndAsync();
-            string path = context.Request.Path;
-            var method = context.Request.Method;
-            // await Calculator.HttpCalculator(method, path, context);
-            await QueryProcessor.ProcessQuery(context, body, path, method);
+            await context.Response.WriteAsync("Hello World!");
+            await PastCode(context);
         });
         app.Run();
+    }
+
+    private static async Task PastCode(HttpContext context)
+    {
+        // await queryString(context);
+        var reader = new StreamReader(context.Request.Body);
+        var body = await reader.ReadToEndAsync();
+        string path = context.Request.Path;
+        var method = context.Request.Method;
+        // await Calculator.HttpCalculator(method, path, context);
+        await QueryProcessor.ProcessQuery(context, body, path, method);
     }
 }
