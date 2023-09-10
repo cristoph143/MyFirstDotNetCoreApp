bdd9aea409418407cbb3f786d1dd83065ddeb020
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-10 22:02:34 +0800

    fix bugs when extracting it to the functions

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
-using Microsoft.Extensions.Primitives;
+// using Microsoft.Extensions.Primitives;
 
-internal class Program
+namespace MyFirstDotNetCoreApp;
+
+internal abstract class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.MapGet("/hello", () => "Hello World!");
         app.Run(async context =>
         {
             // await queryString(context);
@@ -15,139 +17,121 @@ internal class Program
             // var body = await reader.ReadToEndAsync();
             string path = context.Request.Path;
             var method = context.Request.Method;
-
-            if (method == "GET" && path == "/")
-            {
-                int firstNumber = 0, secondNumber = 0;
-
-                if (context.Request.Query.ContainsKey("firstNumber"))
-                {
-                    var firstNumberString = context.Request.Query["firstNumber"][0];
-                    if (string.IsNullOrEmpty(firstNumberString))
-                    {
-                        context.Response.StatusCode = 400;
-                        await context.Response.WriteAsync("Invalid input for 'firstNumber'\n");
-                        return;
-                    }
-
-                    firstNumber = Convert.ToInt32(firstNumberString);
-                }
-                else
-                {
-                    context.Response.StatusCode = 400;
-                    await context.Response.WriteAsync("Invalid input for 'firstNumber'\n");
-                    return;
-                }
-
-                if (context.Request.Query.ContainsKey("secondNumber"))
-                {
-                    var secondNumberString = context.Request.Query["secondNumber"][0];
-                    if (string.IsNullOrEmpty(secondNumberString))
-                    {
-                        context.Response.StatusCode = 400;
-                        await context.Response.WriteAsync("Invalid input for 'secondNumber'\n");
-                        return;
-                    }
-
-                    secondNumber = Convert.ToInt32(secondNumberString);
-                }
-                else
-                {
-                    context.Response.StatusCode = 400;
-                    await context.Response.WriteAsync("Invalid input for 'secondNumber'\n");
-                    return;
-                }
-
-                if (context.Request.Query.ContainsKey("operation"))
-                {
-                    var operation = Convert.ToString(context.Request.Query["operation"][0]);
-
-                    long? result;
-                    switch (operation)
-                    {
-                        case "add":
-                            result = firstNumber + secondNumber;
-                            break;
-                        case "subtract":
-                            result = firstNumber - secondNumber;
-                            break;
-                        case "multiply":
-                            result = firstNumber * secondNumber;
-                            break;
-                        case "divide":
-                            result = secondNumber != 0 ? firstNumber / secondNumber : 0;
-                            break;
-                        case "mod":
-                            result = secondNumber != 0 ? firstNumber % secondNumber : 0;
-                            break;
-                        default:
-                            context.Response.StatusCode = 400;
-                            await context.Response.WriteAsync("Invalid input for 'operation'\n");
-                            return;
-                    }
-
-                    await context.Response.WriteAsync(result.Value.ToString());
-                    await context.Response.CompleteAsync(); // Send the response back to the client
-                }
-                else
-                {
-                    context.Response.StatusCode = 400;
-                    await context.Response.WriteAsync("Invalid input for 'operation'\n");
-                    await context.Response.CompleteAsync(); // Send the response back to the client
-                }
-            }
+            await HttpCalculator(method, path, context);
             // Dictionary<string, StringValues> queryDict =
             //     Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
-            // await queyDict(queryDict, context, body);
+            // await queryDict(queryDict, context, body);
             // await context.Response.WriteAsync("Hello World1!");
             // await queryString(context, path, method);
         });
         app.Run();
     }
 
-    private static async Task queyDict(Dictionary<string, StringValues> queryDict, HttpContext context, string body)
+    private static async Task HttpCalculator(string method, string path, HttpContext context)
     {
-        if (queryDict.ContainsKey("firstName"))
+        if (method == "GET" && path == "/")
         {
-            var firstName = queryDict["firstName"][0];
-            await context.Response.WriteAsync($"<p>Hello World1! {body} ? {queryDict} {firstName}</p>");
+            var isFirst = await IsContainKeys(context, "firstNumber");
+            var isSecond = await IsContainKeys(context, "secondNumber");
+            var isOps = await IsContainKeys(context, "operation");
+
+            if (!isFirst && !isSecond && !isOps) return;
+            await CalculateResult(context);
         }
     }
 
-    private static async Task queryString(HttpContext context, string path, string method)
+    private static async Task CalculateResult(HttpContext context)
     {
-        await context.Response.WriteAsync("Hello World1!");
-        await context.Response.WriteAsync("<h1>Hello World1!</h1>");
-        await context.Response.WriteAsync($"<p>Hello World1! {path} ? {method}</p>");
-        await context.Response.WriteAsync("Hello World2!");
-        switch (method)
-        {
-            case "GET":
-            {
-                var containsAgent = context.Request.Headers.ContainsKey("User-Agent");
-
-                if (containsAgent)
-                {
-                    var userAgent = context.Request.Headers["User-Agent"];
-                    await context.Response.WriteAsync($"<p>Hello World UserAgent! {userAgent}</p>");
-                }
-
-                var containsKeys = context.Request.Query.ContainsKey("id");
-                if (containsKeys)
-                {
-                    var id = context.Request.Query["id"].ToString();
-                    await context.Response.WriteAsync($"<p>Hello World ID! {id}</p>");
-                }
-
-                var containsAuthorizationKey = context.Request.Headers.ContainsKey("AuthorizationKey");
-                if (containsAuthorizationKey)
-                {
-                    var auth = context.Request.Headers["AuthorizationKey"].ToString();
-                    await context.Response.WriteAsync($"<p>Hello World containsAuthorizationKey! {auth}</p>");
-                }
-
-                break;
-            }
-        }
+        var firstNumber = await ConvertToInt(context, "firstNumber");
+        var secondNumber = await ConvertToInt(context, "secondNumber");
+        var operation = context.Request.Query["operation"].FirstOrDefault();       
+        var result = await GetCalculatedResult(context, operation, firstNumber, secondNumber);
+        await context.Response.WriteAsync(result?.ToString() ?? string.Empty);
+        await context.Response.CompleteAsync(); // Send the response back to the client
     }
+
+    private static async Task<bool> IsContainKeys(HttpContext context, string keys)
+    {
+        if (context.Request.Query.ContainsKey(keys)) return true;
+        if (context.Response.HasStarted) return false;
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync($"Invalid input for {keys}\n");
+        await context.Response.CompleteAsync();
+        return false;
+    }
+
+    private static async Task<int> ConvertToInt(HttpContext context, string keys)
+    {
+        var numberString = context.Request.Query[keys].FirstOrDefault();        
+        if (!string.IsNullOrEmpty(numberString)) return Convert.ToInt32(numberString);
+        await HandleInvalidOperation(context, keys);
+        return 0;
+    }
+
+    private static async Task<long?> GetCalculatedResult(HttpContext context, string? operation, int firstNumber, int secondNumber)
+    {
+        return operation switch
+        {
+            "add" => firstNumber + secondNumber,
+            "subtract" => firstNumber - secondNumber,
+            "multiply" => firstNumber * secondNumber,
+            "divide" => secondNumber != 0 ? firstNumber / secondNumber : 0,
+            "mod" => secondNumber != 0 ? firstNumber % secondNumber : 0,
+            _ => await HandleInvalidOperation(context, "operation")
+        };
+    }
+
+    private static async Task<long?> HandleInvalidOperation(HttpContext context, string keys)
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync($"Invalid input for {keys}\n");
+        await context.Response.CompleteAsync();
+        return null;
+    }
+
+    // private static async Task queryDict(Dictionary<string, StringValues> queryDict, HttpContext context, string body)
+    // {
+    //     if (queryDict.ContainsKey("firstName"))
+    //     {
+    //         var firstName = queryDict["firstName"][0];
+    //         await context.Response.WriteAsync($"<p>Hello World1! {body} ? {queryDict} {firstName}</p>");
+    //     }
+    // }
+    //
+    // private static async Task queryString(HttpContext context, string path, string method)
+    // {
+    //     await context.Response.WriteAsync("Hello World1!");
+    //     await context.Response.WriteAsync("<h1>Hello World1!</h1>");
+    //     await context.Response.WriteAsync($"<p>Hello World1! {path} ? {method}</p>");
+    //     await context.Response.WriteAsync("Hello World2!");
+    //     switch (method)
+    //     {
+    //         case "GET":
+    //         {
+    //             var containsAgent = context.Request.Headers.ContainsKey("User-Agent");
+    //
+    //             if (containsAgent)
+    //             {
+    //                 var userAgent = context.Request.Headers["User-Agent"];
+    //                 await context.Response.WriteAsync($"<p>Hello World UserAgent! {userAgent}</p>");
+    //             }
+    //
+    //             var containsKeys = context.Request.Query.ContainsKey("id");
+    //             if (containsKeys)
+    //             {
+    //                 var id = context.Request.Query["id"].ToString();
+    //                 await context.Response.WriteAsync($"<p>Hello World ID! {id}</p>");
+    //             }
+    //
+    //             var containsAuthorizationKey = context.Request.Headers.ContainsKey("AuthorizationKey");
+    //             if (containsAuthorizationKey)
+    //             {
+    //                 var auth = context.Request.Headers["AuthorizationKey"].ToString();
+    //                 await context.Response.WriteAsync($"<p>Hello World containsAuthorizationKey! {auth}</p>");
+    //             }
+    //
+    //             break;
+    //         }
+    //     }
+    // }
 }
