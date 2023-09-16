9b3e6ecb025d50a3fe96d6518eaddd20582f487e
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-16 18:43:41 +0800

    35. GetEndPoint

--- Program.cs
+++ Program.cs
@@ -16,8 +16,27 @@ internal abstract class Program
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddTransient<MyCustomMiddleware>();
         var app = builder.Build();
+        app.Use(async (context, next) =>
+        {
+            Microsoft.AspNetCore.Http.Endpoint? endPoint = context.GetEndpoint();
+            if (endPoint != null)
+            {
+                await context.Response.WriteAsync($"Endpoint: {endPoint.DisplayName}\n");
+            }
+            await next(context);
+        });
         // enable routing
         app.UseRouting();
+        app.Use(async (context, next) =>
+        {
+            Endpoint? endPoint = context.GetEndpoint();
+            if (endPoint != null)
+            {
+                await context.Response.WriteAsync($"Endpoint: {endPoint.DisplayName}\n");
+            }
+            await  next(context);
+        });
+
         // creating end points
         app.UseEndpoints( endpoints =>
         {
