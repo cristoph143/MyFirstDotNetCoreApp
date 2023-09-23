7bbcc1d130e3f3200278686a526ad7417f11d4d2
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-23 19:35:08 +0800

    46. Creating Controllers

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstDotNetCoreApp.Controllers;
+
+public  class HomeController : Controller
+{
+       [Route("sayHello")]
+      public string method1()
+      {
+          return "return from method1";
+      }
+}
