03c19d27fb4045e31ed7c171ae9dda85c51d7dee
Cristopher Bohol <cris.bacus6@gmail.com>
2023-09-24 11:00:03 +0800

    master baseline

--- Program.cs
+++ Program.cs
@@ -1,9 +1,3 @@
-// using MyFirstDotNetCoreApp.CustomConstraints;
-// using MyFirstDotNetCoreApp.CustomMiddleware;
-using MyFirstDotNetCoreApp.PastCodes;
-
-using Microsoft.Extensions.FileProviders;
-
 namespace MyFirstDotNetCoreApp;
 
 internal abstract class Program
@@ -16,20 +10,10 @@ internal abstract class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        // var builder = WebApplication.CreateBuilder(new WebApplicationOptions
-        // { 
-        //     WebRootPath = "myroot"
-        // });       
-        // builder.Services.AddTransient<MyCustomMiddleware>();
-        // //register custom service
-        // builder.Services.AddRouting(options =>
-        // {
-        //     options.ConstraintMap.Add("months", typeof(MonthsCustomConstraints));
-        // });
         var app = builder.Build();
-        // Create an instance of the Past class
-        // var past = new Past();
-        // past.UseWhen(app);
+        // app.UseStaticFiles();
+        // app.UseRouting();
+        // app.MapControllers();
 
         app.Run(async context => { await context.Response.WriteAsync($"Request received at {context.Request.Path}"); });
         app.Run();
