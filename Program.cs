224b503129efdaf07b184d62ab54da190433a893
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-25 20:34:31 +0800

    71. Input Formatters

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ internal abstract class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllers();
+        builder.Services.AddControllers().AddXmlSerializerFormatters();
+
         var app = builder.Build();
         app.UseStaticFiles();
         app.UseRouting();
