a2b733b833ccf8a00c4dc181005246aef86ad5a2
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-30 22:04:36 +0800

    78. Views

---
+++ Controllers/HomeControllers.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstDotNetCoreApp.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("home")]
+    public IActionResult Index()
+    {
+        return View(); //Views/Home/Index.cshtml
+        //return View("abc"); //abc.cshtml
+        //return new ViewResult() { ViewName = "abc" };
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,13 +10,12 @@ internal abstract class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddControllers();
+        builder.Services.AddControllersWithViews();
         var app = builder.Build();
         app.UseStaticFiles();
         app.UseRouting();
         app.MapControllers();
 
-        app.Run(async context => { await context.Response.WriteAsync($"Request received at {context.Request.Path}"); });
         app.Run();
     }
 }
