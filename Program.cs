9ea4f411251769bc4f08521f9dcc7cb7c881f08a
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-10-04 17:16:39 +0800

    baseline

--- Program.cs
+++ Program.cs
@@ -10,13 +10,11 @@ internal abstract class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddControllers();
-        var app = builder.Build();
+builder.Services.AddControllersWithViews();        var app = builder.Build();
         app.UseStaticFiles();
         app.UseRouting();
         app.MapControllers();
 
-        app.Run(async context => { await context.Response.WriteAsync($"Request received at {context.Request.Path}"); });
         app.Run();
     }
 }
