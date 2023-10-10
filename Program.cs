ba8772f7bdbbb5d3b8126be7db1d4712d6912339
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-10-10 16:49:22 +0800

    126. AddTransient(), AddScoped(), AddSingleton()

--- Program.cs
+++ Program.cs
@@ -15,13 +15,16 @@ internal abstract class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllersWithViews();
-        builder.Services.Add(new ServiceDescriptor(
-            typeof(ICitiesService),
-            typeof(CitiesService),
-            // ServiceLifetime.Transient
-            // ServiceLifetime.Singleton
-            ServiceLifetime.Scoped
-        ));
+        // builder.Services.Add(new ServiceDescriptor(
+        //     typeof(ICitiesService),
+        //     typeof(CitiesService),
+        //     // ServiceLifetime.Transient
+        //     // ServiceLifetime.Singleton
+        //     ServiceLifetime.Scoped
+        // ));
+        //builder.Services.AddTransient<ICitiesService, CitiesService>();
+        builder.Services.AddScoped<ICitiesService, CitiesService>();
+        //builder.Services.AddSingleton<ICitiesService, CitiesService>();        var app = builder.Build();
         var app = builder.Build();
         app.UseStaticFiles();
         app.UseRouting();
