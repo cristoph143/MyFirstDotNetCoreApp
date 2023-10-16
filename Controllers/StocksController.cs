a19792f48468c6e3ab95544823d01716a24529e3
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-10-16 22:10:38 +0800

    need to fix Stockname not fetch

--- Controllers/StocksController.cs
+++ Controllers/StocksController.cs
@@ -7,16 +7,17 @@ namespace MyFirstDotNetCoreApp.Controllers;
 
 [Route("[controller]")]
 
-public class StocksController(FinnhubService finnhubService, IOptions<TradingOptions> tradingOptions)
-    : Controller
+public class StocksController(
+        FinnhubService finnhubService, 
+        IOptions<TradingOptions> tradingOptions,
+        IConfiguration configuration
+        ): Controller
 {
     [Route("/stocks")]
     public async Task<IActionResult> Index()
     {
         tradingOptions.Value.DefaultStockSymbol ??= "MSFT";
-
         Dictionary<string, object>? responseDictionary = await finnhubService.GetStockPriceQuote(tradingOptions.Value.DefaultStockSymbol);
-
         Stock stock = new Stock
         {
             StockSymbol = tradingOptions.Value.DefaultStockSymbol,
@@ -25,7 +26,33 @@ public class StocksController(FinnhubService finnhubService, IOptions<TradingOpt
             LowestPrice = Convert.ToDouble(responseDictionary["l"].ToString()),
             OpenPrice = Convert.ToDouble(responseDictionary["o"].ToString())
         };
-
         return View(stock);
+    }
+
+    private readonly TradingOptions _tradingOptions = tradingOptions.Value;
+
+    [Route("/stock-trade")]
+    [Route("[action]")]
+    [Route("~/[controller]")]
+    public async Task<IActionResult> Trade()
+    {
+        tradingOptions.Value.DefaultStockSymbol ??= "MSFT";
+        Dictionary<string, object>? companyProfileDictionary =
+            finnhubService.GetCompanyProfile(_tradingOptions.DefaultStockSymbol);
+        Dictionary<string, object>? stockQuoteDictionary =
+            await finnhubService.GetStockPriceQuote(_tradingOptions.DefaultStockSymbol);
+        StockTrade stockTrade = new StockTrade { StockSymbol = _tradingOptions.DefaultStockSymbol };
+        if (companyProfileDictionary != null && stockQuoteDictionary != null)
+        {
+            if (companyProfileDictionary.TryGetValue("ticker", out var value))
+                stockTrade.StockSymbol = Convert.ToString(value);
+            if (companyProfileDictionary.TryGetValue("name", out var value1))
+                stockTrade.StockName = Convert.ToString(value1);
+            if (stockQuoteDictionary.TryGetValue("c", out var value2))
+                stockTrade.Price = Convert.ToDouble(value2.ToString() ?? "0");
+        }
+
+        ViewBag.FinnhubToken = configuration["FinnhubToken"];
+        return View(stockTrade);
     }
 }
